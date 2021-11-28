a7c138b87efcae0fc4b5fc876ec5f4266cc61aad
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-11-28 15:00:33 +1300

    Added bot that signs in via basic tutorial

---
+++ BrothermanBill/DiscordWrapper.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrothermanBill
+{
+    internal class DiscordWrapper
+    {
+        private DiscordSocketClient SocketClient;
+        private string Token;
+
+        public DiscordWrapper(string token)
+        {
+            Token = token;
+            SocketClient = new DiscordSocketClient();
+            SocketClient.Log += Log;
+        }
+
+        public async Task Run()
+        {
+            await SocketClient.LoginAsync(TokenType.Bot, Token);
+            await SocketClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- BrothermanBill/Program.cs
+++ BrothermanBill/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BrothermanBill;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .AddUserSecrets<Program>()
+                 .Build();
+
 Console.WriteLine("Hello, World!");
+
+var discord = new DiscordWrapper("");
+await discord.Run();
