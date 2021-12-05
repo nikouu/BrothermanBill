f405850e2cb52e94ac7d899c108d27368b36d2d0
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-12-05 20:10:32 +1300

    Saving module progress for another day

--- BrothermanBill/Program.cs
+++ BrothermanBill/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BrothermanBill;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var config = new ConfigurationBuilder()
                  .SetBasePath(Directory.GetCurrentDirectory())
                  .AddJsonFile("appsettings.json")
                  .AddUserSecrets<Program>()
                  .Build();
+
+// https://andrewlock.net/exploring-dotnet-6-part-10-new-dependency-injection-features-in-dotnet-6/
+var services = new ServiceCollection().AddSingleton<InfoModule>();
+
+
 
 Console.WriteLine("Hello, World!");
 
