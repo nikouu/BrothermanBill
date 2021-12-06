34eb58add63e3c33d60d279b543861ff4288613c
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-12-06 18:09:17 +1300

    First words 🙏

--- BrothermanBill/Program.cs
+++ BrothermanBill/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BrothermanBill;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
+// this could also be the new .net6 ConfigurationManager
 var config = new ConfigurationBuilder()
                  .SetBasePath(Directory.GetCurrentDirectory())
                  .AddJsonFile("appsettings.json")
@@ -11,11 +16,34 @@ var config = new ConfigurationBuilder()
                  .Build();
 
 // https://andrewlock.net/exploring-dotnet-6-part-10-new-dependency-injection-features-in-dotnet-6/
-var services = new ServiceCollection().AddSingleton<InfoModule>();
+await using var services = new ServiceCollection()
+    .AddSingleton<DiscordSocketClient>()
+    .AddSingleton<CommandService>()
+    .AddSingleton<CommandHandler>()
+    .AddSingleton<InfoModule>()
+    .BuildServiceProvider();
 
+var commands = services.GetRequiredService<CommandService>();
+var socketClient = services.GetRequiredService<DiscordSocketClient>();
+var commandHandler = services.GetRequiredService<CommandHandler>();
+
+//await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+
+await commandHandler.InstallCommandsAsync();
+
+
+socketClient.Log += (LogMessage msg) => {
+    Console.WriteLine(msg.ToString());
+    return Task.CompletedTask;
+};
+//var discord = new DiscordWrapper();
+//await discord.Run();
+
+await socketClient.LoginAsync(TokenType.Bot, config["DiscordBotToken"]);
+await socketClient.StartAsync();
 
 
 Console.WriteLine("Hello, World!");
 
-var discord = new DiscordWrapper(config["DiscordBotToken"]);
-await discord.Run();
+
+await Task.Delay(Timeout.Infinite);
