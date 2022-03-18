709a5176c2007eb8335e7af67bf5c28b993cfa27
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-03-19 11:29:44 +1300

    Added checks before service removal

--- BrothermanBill/stop.sh
+++ BrothermanBill/stop.sh
@@ -5,4 +5,23 @@ pkill -f "Lavalink.jar"
 
 echo "Process killed" #stops STDOUT from hanging and causing error
 
-sudo systemctl disable brothermanbill.service
+# https://superuser.com/questions/513159/how-to-remove-systemd-services
+
+if systemctl is-active --quiet brothermanbill.service; then
+	sudo systemctl stop brothermanbill.service
+fi
+
+if systemctl is-enabled --quiet brothermanbill.service; then
+	sudo systemctl disable brothermanbill.service
+fi
+
+if [ -f "/etc/systemd/system/brothermanbill.service" ] ; then
+    rm "/etc/systemd/system/brothermanbill.service"
+fi
+
+if [ -f "/usr/lib/systemd/system/brothermanbill.service " ] ; then
+    rm "/usr/lib/systemd/system/brothermanbill.service "
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
